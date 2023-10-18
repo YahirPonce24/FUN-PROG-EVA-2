fb4a3f75f797955fa129a96929e7a89ec198a446
Yahir Ponce <142949341+YahirPonce24@users.noreply.github.com>
2023-10-18 17:41:07 -0600

    Add files via upload

---
+++ EVA2_8_ELSE_IF/src/eva2_8_else_if/EVA2_8_ELSE_IF.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_8_else_if;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_8_ELSE_IF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int mes;
+        Scanner sc =  new Scanner (System.in);
+        System.out.println("Introduce el número de mes: ");
+        mes = sc.nextInt();
+        /*if ( (mes >= 1 ) && ( mes <= 12 ) )  // ( mes > 0 ) && ( mes < 12 ) 
+        {
+            System.out.println("El número "+mes+" es un mes válido!!!");
+        */
+        
+            if ( mes == 1 )
+                System.out.println("ENERO");
+            else if (mes == 2)
+                System.out.println("FEBRERO");
+            else if (mes == 3)
+                System.out.println("MARZO");
+            else if (mes == 4)
+                System.out.println("ABRIL");
+            else if (mes == 5)
+                System.out.println("MAYO");
+            else if (mes == 6)
+                System.out.println("JUNIO");
+            else if (mes == 7)
+                System.out.println("JULIO");
+            else if (mes == 8)
+                System.out.println("AGOSTO");
+            else if (mes == 9)
+                System.out.println("SEPTIEMBRE");
+            else if (mes == 10)
+                System.out.println("OCTUBRE");
+            else if (mes == 11)
+                System.out.println("NOVIEMBRE");
+            else if (mes == 12)
+                System.out.println("DICIEMBRE");
+            else
+                System.out.println("El número " + mes + " no es un mes válido");
+        /*}
+         else
+            System.out.println("El número "+mes+" no es un mes válido!!!");
+         */   
+            
+        
+        
+        
+    }
+    
+}
