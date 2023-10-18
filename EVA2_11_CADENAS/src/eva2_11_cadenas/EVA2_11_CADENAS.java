5c67df7a23676932e660fc5be9b87e476db51345
Yahir Ponce <142949341+YahirPonce24@users.noreply.github.com>
2023-10-18 17:58:37 -0600

    Add files via upload

---
+++ EVA2_11_CADENAS/src/eva2_11_cadenas/EVA2_11_CADENAS.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_11_cadenas;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_11_CADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String cade = "Hola";
+        if (cade.equals("Hola"))
+            System.out.println("LAS CADENAS SON IGUALES");
+        else
+            System.out.println("LAS CADENAS SON DIFERENTES");
+        
+    }
+    
+}
